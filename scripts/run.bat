8f4e2f855edb0896ad1b83ee07edd0c2b48370cf
David C <45155292+DavoDC@users.noreply.github.com>
2026-03-17 23:00:04 +0800

    Add batch file for easy running

---
+++ scripts/run.bat
@@ -0,0 +1,2 @@
+@echo off
+wt.exe -p "Git Bash" -d "C:\Users\David\GitHubRepos\CompilationVidMaker" "C:\Program Files\Git\bin\bash.exe" --login -i "/c/Users/David/GitHubRepos/CompilationVidMaker/scripts/run.sh"
---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /c/Users/David/GitHubRepos/CompilationVidMaker
+python src/main.py
+echo ""
+echo "Done. Press any key to exit."
+read -n 1
